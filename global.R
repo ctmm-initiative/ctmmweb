0576399ae5fc6f4459b4b6bffb3ea1624d84cf83
Xianghui Dong <xhdong@umd.edu>
2017-04-27 13:12:22 -0400

    improve log slider add number for plots in visualization page

--- global.R
+++ global.R
@@ -54,3 +54,38 @@ click_help <- function(input, output, session, title, file){
     ))
   })
 }
+# log slider ----
+JS.logify <- function(digits = 2) {
+  paste0(  "
+           // function to logify a sliderInput
+           function logifySlider (sliderId, sci = false) {
+           if (sci) {
+           // scientific style
+           $('#'+sliderId).data('ionRangeSlider').update({
+           'prettify': function (num) { return ('10<sup>'+num+'</sup>'); }
+           })
+           } else {
+           // regular number style
+           $('#'+sliderId).data('ionRangeSlider').update({
+           'prettify': function (num) { return (Math.pow(10, num).toFixed(",
+           digits, ")); }
+           })
+           }
+           }")
+}
+
+# call logifySlider for each relevant sliderInput
+JS.onload <- function(slider_id_vec, sci = FALSE) {
+  slider_call <- function(slider_id) {
+    paste0("logifySlider('", slider_id,
+           "', sci = ", ifelse(sci, "true", "false") , ")")
+  }
+  return(paste0("
+                // execute upon document loading
+                $(document).ready(function() {
+                // wait a few ms to allow other scripts to execute
+                setTimeout(function() {",
+                paste0(lapply(slider_id_vec, slider_call), collapse = "\n"),
+                "}, 5)})"
+                ))
+                }
