0a96cff3c632770a9c77e722c8a06d3452e382ee
Xianghui Dong <xhdong@umd.edu>
2017-09-15 11:06:59 -0400

    add crayon github install to run.R

--- run.R
+++ run.R
@@ -3,5 +3,6 @@ if (!require("devtools"))
   install.packages("devtools")
 devtools::install_github("rstudio/shiny")
 devtools::install_github("ctmm-initiative/ctmm")
+devtools::install_github("r-lib/crayon")
 # run app
 shiny::runGitHub('ctmm-initiative/ctmm-webapp')
