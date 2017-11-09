ed49dda0b0a9bee1955f6b04efc7c06f82f5f38b
Xianghui Dong <xhdong@umd.edu>
2017-11-09 14:25:14 -0500

    hosted app seemed working with syncing bounds

--- run.R
+++ run.R
@@ -2,6 +2,7 @@
 if (!require("devtools"))
   install.packages("devtools")
 devtools::install_github("rstudio/shiny")
+devtools::install_github("rstudio/leaflet")
 devtools::install_github("ctmm-initiative/ctmm")
 devtools::install_github("r-lib/crayon")
 # run app
