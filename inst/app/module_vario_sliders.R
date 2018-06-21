67ed84202b5a6e3c7a3a1f8978dc81793adf71d4
Xianghui Dong <xhdong@umd.edu>
2018-06-21 17:03:19 -0400

    vario sliders module should be working

--- inst/app/module_vario_sliders.R
+++ inst/app/module_vario_sliders.R
@@ -1,10 +1,11 @@
-# abstract original code into module so it can be reused. need to support two curves.
-vario_sliders_Input <- function(id, dialog_title) {
+# abstract original code into module so it can be reused. need to support two curves. Need to use camelcase naming convention
+# previously slider id is built with vfitz_sigma, vfitz_z, vfitz_tau1 etc. now with module id, should use slider name directly.
+varioSlidersInput <- function(id, dialog_title) {
   ns <- NS(id)
   modalDialog(title = dialog_title,
               fluidRow(column(4, uiOutput(ns("fit_sliders"))),
                        column(8, plotOutput(ns("fit_plot"))),
-                       column(4, offset = 2, uiOutput(ns("fit_zoom")))),
+                       column(4, offset = 6, uiOutput(ns("fit_zoom")))),
               size = "l",
               footer = fluidRow(
                 column(3, actionButton(ns("center_slider"),
@@ -18,13 +19,60 @@ vario_sliders_Input <- function(id, dialog_title) {
                                     style = ctmmweb:::STYLES$page_action)))
   )
 }
-# get slider info from vario, ctmm_obj. fraction is the internal value of zoom slider in vario control box. id_prefix need a _ in end. for our purpose, it's easier to further wrap input processing code here, only provide the original input value in parameter.
-get_sliders_info <- function(vario_id, vario_list, ctmm_obj_list,
+# sliders id are created in get_sliders_info with module_id, so we don't need to get ns from session$ns, just make sure module_id is same with id parameter
+varioSliders <- function(input, output, session, sliders_info) {
+  # init sliders ----
+  output$fit_sliders <- renderUI({
+    req(sliders_info()$control_sliders)
+  })
+  output$fit_zoom <- renderUI({
+    # js function need to know the id of zoom slider, which is also used in plot code. logify is also needed.
+    list(tags$head(tags$script(HTML(ctmmweb::JS.logify(3)))),
+         tags$head(tags$script(HTML(ctmmweb::JS.onload(session$ns("z"))))),
+         req(sliders_info()$zoom_slider))
+  })
+  # get_current_ctmm()----
+  # convert current slider value to ctmm object. also returned by module server function. apply button will take that to update app global variable
+  get_current_ctmm <- reactive({
+    # there is a time when sliders are initialized but without value, then later storer call get NULL parameters
+    # cat(input$sigma)
+    req(!is.null(input$sigma))
+    # slider names can be dynamic depend on data
+    slider_values <- lapply(sliders_info()$control_dt$name,
+                            function(x) {
+                              input[[x]]
+                            })
+    names(slider_values) <- sliders_info()$control_dt$name
+    CTMM <- do.call(sliders_info()$STUFF$storer, slider_values)
+  })
+  # update plot by sliders
+  output$fit_plot <- renderPlot({
+    # req(slider_to_CTMM())  # otherwise error: replacement of length zero
+    # within module we can access input without ns, it will be converted. ns is only used in creating ui items.
+    plot(sliders_info()$vario, CTMM = req(get_current_ctmm()),
+         col.CTMM = "green", fraction = 10 ^ input$z)
+  })
+  # center sliders ----
+  observeEvent(input$center_slider, {
+    adjust_slider <- function(name) {
+      # Shiny will complain for named vector
+      # id <- paste0("vfit_", name)
+      # error slider usually have initial value of 0, double that will get 0.
+      if (input[[name]] != 0) {
+        updateSliderInput(session, name,
+                          max = round(input[[name]] * 2, 2))
+      }
+    }
+    lapply(sliders_info()$control_dt$name, adjust_slider)
+  })
+  return(get_current_ctmm)
+
+}
+
+# get slider info from vario, ctmm_obj. fraction is the internal value of zoom slider in vario control box. module using id- as prefix. for our purpose, it's easier to further wrap input processing code here, only provide the original input value in parameter.
+get_sliders_info <- function(vario, ctmm_obj,
                             fraction_internal_value,
-                            slider_id_prefix) {
-  ids <- names(vario_list)
-  vario <- vario_list[ids == vario_id][[1]]
-  ctmm_obj <- ctmm_obj_list[ids == vario_id][[1]]
+                            module_id) {
   fraction_face_value <- 10 ^ fraction_internal_value
   STUFF <- ctmm:::variogram.fit.backend(vario, CTMM = ctmm_obj,
                                         fraction = fraction_face_value, b = 10)
@@ -41,7 +89,7 @@ get_sliders_info <- function(vario_id, vario_list, ctmm_obj_list,
   }
   slider_list <- lapply(1:nrow(dt), function(i) {
     sliderInput(
-      inputId = paste0(slider_id_prefix, dt[i, name]),
+      inputId = paste0(module_id, "-", dt[i, name]),
       label = dt[i, label], min = round(dt[i, min], 3),
       max = round(dt[i, max], 3), value = round(dt[i, initial], 3),
       step = dt[i, step])
@@ -52,61 +100,3 @@ get_sliders_info <- function(vario_id, vario_list, ctmm_obj_list,
               control_sliders = slider_list[names(slider_list) != "z"],
               zoom_slider = slider_list[names(slider_list) == "z"]))
 }
-# use reactive to get current vario, ctmm_obj. this will be duplicated on 2 tabs, make the code as simple as possible
-guess_slider_values <- reactive({
-  get_sliders_info(input$tune_selected,
-                   req(select_data_vario()$vario_list),
-                   values$selected_data_guess_list,
-                   input$zoom_lag_fraction,
-                   "tune_guess_")
-})
-
-
-
-# init control sliders
-output$fit_sliders <- renderUI({
-  req(init_slider_values()$control_sliders)
-})
-output$fit_zoom <- renderUI({
-  list(tags$head(tags$script(HTML(ctmmweb::JS.onload("vfit_z")))),
-       req(init_slider_values()$zoom_slider))
-})
-observeEvent(input$center_slider, {
-  adjust_slider <- function(name) {
-    # Shiny will complain for named vector
-    id <- paste0("vfit_", name)
-    # error slider usually have initial value of 0, double that will get 0.
-    if (input[[id]] != 0) {
-      updateSliderInput(session, id,
-                        max = round(input[[id]] * 2, 2))
-    }
-  }
-  lapply(init_slider_values()$control_dt$name, adjust_slider)
-})
-# the convert function is already wrapped, need to get slider names and input values properly, which can be dynamic.
-slider_to_CTMM <- reactive({
-  # there is a time when sliders are initialized but without value, then later storer call get NULL parameters
-  req(!is.null(input$vfit_sigma))
-  slider_values <- lapply(init_slider_values()$control_dt$name,
-                          function(x) {
-                            input[[paste0("vfit_", x)]]
-                          })
-  names(slider_values) <- init_slider_values()$control_dt$name
-  CTMM <- do.call(init_slider_values()$STUFF$storer, slider_values)
-})
-# update plot by sliders ----
-output$fit_plot <- renderPlot({
-  req(slider_to_CTMM())  # otherwise error: replacement of length zero
-  fraction <- 10 ^ input$vfit_z
-  plot(init_slider_values()$vario, CTMM = slider_to_CTMM(),
-       col.CTMM = "green", fraction = fraction)
-})
-observeEvent(input$tuned, {
-  # LOG fine tune apply
-  log_msg("Apply Fine-tuned Parameters")
-  removeModal()
-  ids <- sapply(select_data_vario()$vario_list,
-                function(vario) vario@info$identity)
-  values$selected_data_guess_list[ids == input$tune_selected][[1]] <-
-    slider_to_CTMM()
-})
