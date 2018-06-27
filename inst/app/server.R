f5fc7a272dfbeda580c3b4ecfa3125453969695f
Xianghui Dong <xhdong@umd.edu>
2018-06-27 11:32:27 -0400

    slider module worked for tab 1

--- inst/app/server.R
+++ inst/app/server.R
@@ -435,7 +435,9 @@ output:
     # importing should always move to visualization page.
     shinydashboard::updateTabItems(session, "tabs", "plots")
   }
-  # app(shiny_app_data) ----
+  # 0 app(shiny_app_data) ----
+  ## need to put this after import code as it will call import functions.
+  ## APP_wd: app loading directory could be package installation folder, or server.R folder depend on loading method.
   # app can be launched from rstudio on server.R directly(i.e. runshinydir for app folder, used to be the run.R method), or from package function app(). Need to detect launch mode first, then detect app() parameters if in app mode. By checking environment strictly, same name object in global env should not interfer with app.
   # if app started from starting server.R, current env 2 level parent is global, because 1 level parent is server function env. this is using parent.env which operating on env. parent.frame operating on function call stack, which could be very deep, sys.nframe() reported 37 in browser call, sys.calls give details, the complex shiny maintaince stack.
   # run() function env if called from ctmmweb::app(), one level down from global if run server.R in Rstudio
@@ -471,6 +473,8 @@ output:
     # cat("running in runShinydir mode\n")
     APP_wd <- "."
   }
+  # load sliders module, as APP_wd is needed. it's dynamic code in server side, so no need to load in global
+  source(file.path(APP_wd, "module_vario_sliders.R"))
   # upload dialog
   observeEvent(input$tele_file, {
     req(input$tele_file)
@@ -1835,105 +1839,130 @@ output:
     if (input$tune_selected != "") {
       # LOG fine tune start
       log_msg("Fine-tune Parameters for", input$tune_selected)
-      showModal(modalDialog(title = paste0("Fine-tune parameters for ",
-                                           input$tune_selected),
-                      fluidRow(column(4, uiOutput("fit_sliders")),
-                               column(8, plotOutput("fit_plot")),
-                               column(4, offset = 2, uiOutput("fit_zoom"))),
-                      size = "l",
-                      footer = fluidRow(
-        column(3, actionButton("center_slider", "Center current sliders",
-                               icon = icon("align-center"))),
-        column(3, offset = 2,
-               modalButton("Cancel", icon = icon("ban"))),
-        column(2, offset = 2,
-               actionButton("tuned", "Apply",
-                            icon = icon("check"),
-                            style = ctmmweb:::STYLES$page_action)))
-                            ))
+      # showModal(modalDialog(title = paste0("Fine-tune parameters for ",
+      #                                      input$tune_selected),
+      #                 fluidRow(column(4, uiOutput("fit_sliders")),
+      #                          column(8, plotOutput("fit_plot")),
+      #                          column(4, offset = 2, uiOutput("fit_zoom"))),
+      #                 size = "l",
+      #                 footer = fluidRow(
+      #   column(3, actionButton("center_slider", "Center current sliders",
+      #                          icon = icon("align-center"))),
+      #   column(3, offset = 2,
+      #          modalButton("Cancel", icon = icon("ban"))),
+      #   column(2, offset = 2,
+      #          actionButton("tuned", "Apply",
+      #                       icon = icon("check"),
+      #                       style = ctmmweb:::STYLES$page_action)))
+      #                       ))
+      showModal(varioSlidersInput("tune_guess",
+                                  paste0("Fine-tune parameters for ",
+                                         input$tune_selected)))
     }
   })
-  # init values of sliders ----
-  init_slider_values <- reactive({
+  # guess sliders ----
+  guess_slider_values <- reactive({
+    # TODO vario list, ctmm_obj_list name may not be animal name. there could be multi models for same animal, the drop down list need to be model name, then need to map to vario by animal name
     vario_list <- req(select_data_vario()$vario_list)
-    ids <- names(vario_list)
-    vario <- vario_list[ids == input$tune_selected][[1]]
-    CTMM <- values$selected_data_guess_list[ids == input$tune_selected][[1]]
-    fraction <- 10 ^ input$zoom_lag_fraction
-    STUFF <- ctmm:::variogram.fit.backend(vario, CTMM = CTMM,
-                                          fraction = fraction, b = 10)
-    dt <- data.table(STUFF$DF)
-    dt[, name := row.names(STUFF$DF)]
-    # zoom slider used different base, and minus 1 from min,max.
-    dt[name == "z", c("min", "max") := list(min - 1, max - 1)]
-    # initial is taken from last page control directly
-    dt[name == "z", initial := input$zoom_lag_fraction]
-    # didn't use the step value in ctmm. use 0.001 instead, but need to adjust for error when data is calibrated
-    dt[, step := 0.001]
-    if ("MSE" %in% names(vario)) {
-      dt[name == "error", step := 1]
-    }
-    slider_list <- lapply(1:nrow(dt), function(i) {
-      sliderInput(
-        inputId = paste0("vfit_", dt[i, name]),
-        label = dt[i, label], min = round(dt[i, min], 3),
-        max = round(dt[i, max], 3), value = round(dt[i, initial], 3),
-        step = dt[i, step])
-    })
-    names(slider_list) <- dt$name
-
-    # zoom is for view only, separate it from others
-    return(list(vario = vario, STUFF = STUFF,
-                control_dt = dt[name != "z"],
-                control_sliders = slider_list[names(slider_list) != "z"],
-                zoom_slider = slider_list[names(slider_list) == "z"]))
+    vario_id <- input$tune_selected
+    vario_names <- names(vario_list)
+    vario <- vario_list[vario_names == vario_id][[1]]
+    ctmm_obj <- values$selected_data_guess_list[vario_names == vario_id][[1]]
+    get_sliders_info(vario, ctmm_obj, input$zoom_lag_fraction, "tune_guess")
   })
+  guess_ctmm <- callModule(varioSliders, "tune_guess", guess_slider_values, ctmm_colors[1:2])
+  # init values of sliders ---
+  # init_slider_values <- reactive({
+  #   vario_list <- req(select_data_vario()$vario_list)
+  #   ids <- names(vario_list)
+  #   vario <- vario_list[ids == input$tune_selected][[1]]
+  #   CTMM <- values$selected_data_guess_list[ids == input$tune_selected][[1]]
+  #   fraction <- 10 ^ input$zoom_lag_fraction
+  #   STUFF <- ctmm:::variogram.fit.backend(vario, CTMM = CTMM,
+  #                                         fraction = fraction, b = 10)
+  #   dt <- data.table(STUFF$DF)
+  #   dt[, name := row.names(STUFF$DF)]
+  #   # zoom slider used different base, and minus 1 from min,max.
+  #   dt[name == "z", c("min", "max") := list(min - 1, max - 1)]
+  #   # initial is taken from last page control directly
+  #   dt[name == "z", initial := input$zoom_lag_fraction]
+  #   # didn't use the step value in ctmm. use 0.001 instead, but need to adjust for error when data is calibrated
+  #   dt[, step := 0.001]
+  #   if ("MSE" %in% names(vario)) {
+  #     dt[name == "error", step := 1]
+  #   }
+  #   slider_list <- lapply(1:nrow(dt), function(i) {
+  #     sliderInput(
+  #       inputId = paste0("vfit_", dt[i, name]),
+  #       label = dt[i, label], min = round(dt[i, min], 3),
+  #       max = round(dt[i, max], 3), value = round(dt[i, initial], 3),
+  #       step = dt[i, step])
+  #   })
+  #   names(slider_list) <- dt$name
+  #
+  #   # zoom is for view only, separate it from others
+  #   return(list(vario = vario, STUFF = STUFF,
+  #               control_dt = dt[name != "z"],
+  #               control_sliders = slider_list[names(slider_list) != "z"],
+  #               zoom_slider = slider_list[names(slider_list) == "z"]))
+  # })
   # init control sliders
-  output$fit_sliders <- renderUI({
-    req(init_slider_values()$control_sliders)
-  })
-  output$fit_zoom <- renderUI({
-    list(tags$head(tags$script(HTML(ctmmweb::JS.onload("vfit_z")))),
-         req(init_slider_values()$zoom_slider))
-  })
-  observeEvent(input$center_slider, {
-    adjust_slider <- function(name) {
-      # Shiny will complain for named vector
-      id <- paste0("vfit_", name)
-      # error slider usually have initial value of 0, double that will get 0.
-      if (input[[id]] != 0) {
-        updateSliderInput(session, id,
-                          max = round(input[[id]] * 2, 2))
-      }
-    }
-    lapply(init_slider_values()$control_dt$name, adjust_slider)
-  })
-  # current CTMM according to sliders
-  slider_to_CTMM <- reactive({
-    # there is a time when sliders are initialized but without value, then later storer call get NULL parameters
-    req(!is.null(input$vfit_sigma))
-    slider_values <- lapply(init_slider_values()$control_dt$name,
-                            function(x) {
-                              input[[paste0("vfit_", x)]]
-    })
-    names(slider_values) <- init_slider_values()$control_dt$name
-    CTMM <- do.call(init_slider_values()$STUFF$storer, slider_values)
-  })
-  # update plot by sliders ----
-  output$fit_plot <- renderPlot({
-    req(slider_to_CTMM())  # otherwise error: replacement of length zero
-    fraction <- 10 ^ input$vfit_z
-    plot(init_slider_values()$vario, CTMM = slider_to_CTMM(),
-         col.CTMM = "green", fraction = fraction)
-  })
-  observeEvent(input$tuned, {
+  # output$fit_sliders <- renderUI({
+  #   req(init_slider_values()$control_sliders)
+  # })
+  # output$fit_zoom <- renderUI({
+  #   list(tags$head(tags$script(HTML(ctmmweb::JS.onload("vfit_z")))),
+  #        req(init_slider_values()$zoom_slider))
+  # })
+  # observeEvent(input$center_slider, {
+  #   adjust_slider <- function(name) {
+  #     # Shiny will complain for named vector
+  #     id <- paste0("vfit_", name)
+  #     # error slider usually have initial value of 0, double that will get 0.
+  #     if (input[[id]] != 0) {
+  #       updateSliderInput(session, id,
+  #                         max = round(input[[id]] * 2, 2))
+  #     }
+  #   }
+  #   lapply(init_slider_values()$control_dt$name, adjust_slider)
+  # })
+  # # current CTMM according to sliders
+  # slider_to_CTMM <- reactive({
+  #   # there is a time when sliders are initialized but without value, then later storer call get NULL parameters
+  #   req(!is.null(input$vfit_sigma))
+  #   slider_values <- lapply(init_slider_values()$control_dt$name,
+  #                           function(x) {
+  #                             input[[paste0("vfit_", x)]]
+  #   })
+  #   names(slider_values) <- init_slider_values()$control_dt$name
+  #   CTMM <- do.call(init_slider_values()$STUFF$storer, slider_values)
+  # })
+  # # update plot by sliders ---
+  # output$fit_plot <- renderPlot({
+  #   req(slider_to_CTMM())  # otherwise error: replacement of length zero
+  #   fraction <- 10 ^ input$vfit_z
+  #   plot(init_slider_values()$vario, CTMM = slider_to_CTMM(),
+  #        col.CTMM = "green", fraction = fraction)
+  # })
+  # apply tune ----
+  # observeEvent(input$tuned, {
+  #   # LOG fine tune apply
+  #   log_msg("Apply Fine-tuned Parameters")
+  #   removeModal()
+  #   ids <- sapply(select_data_vario()$vario_list,
+  #                 function(vario) vario@info$identity)
+  #   values$selected_data_guess_list[ids == input$tune_selected][[1]] <-
+  #     slider_to_CTMM()
+  # })
+  # - is not valid in symbol
+  observeEvent(input$`tune_guess-tuned`, {
     # LOG fine tune apply
     log_msg("Apply Fine-tuned Parameters")
     removeModal()
     ids <- sapply(select_data_vario()$vario_list,
                   function(vario) vario@info$identity)
     values$selected_data_guess_list[ids == input$tune_selected][[1]] <-
-      slider_to_CTMM()
+      guess_ctmm()
   })
   # fine tune sliders > ----
   # p5. model selection ----
