2323f4e25bb2ea79c6f77714bf51bb042eacb1ff
Xianghui Dong <xhdong@umd.edu>
2019-01-30 14:10:16 -0500

    move shinydashboard and DT to package dependency, so p_load in global only load package, not installing them.

--- inst/app/global.R
+++ inst/app/global.R
@@ -7,7 +7,7 @@ if (!require("pacman")) install.packages("pacman")
 # packrat doesn't recognize p_load_gh so need to put ctmm in p_load again. because deployment try to reproduce current environment, it will install ctmm github version even I only used p_load here since I have github version installed. and the run script will install github version first so it also work.
 # pipe operator is imported by DT, stringr, leaflet, etc. loading DT enable using of pipe
 # the reason to load first line of packages is that they are not really needed for package functions, so are not included in package dependencies. we still need p_load to install them in first run. since they are loaded, no need to use full qualified name in app code.
-pacman::p_load(shiny, shinydashboard, DT,
+pacman::p_load(shiny, shinydashboard,
                ctmm, data.table)
 # package installation time in current time zone. This is used in ui (app info dialog) and server (start info). previously in here to save 2 calls, which cost 500 ms. but this often lead to error when app is stopped in debugging, which caused many re-launch, way more than the 500 ms saved. instead, write message menu in server side dynamically once.
 # PKG_INSTALLATION_TIME <- format(file.mtime(system.file("app", package = "ctmmweb")), usetz = TRUE)
